bf56e525b3e76f3e253db41651c48a8f7e807a89
Benjamin Sterling Silver <bsilver47@outlook.com>
2026-01-16 16:00:59 -0700

    Updated game to allow for repeating with best/high score being reported at the end

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,11 @@ use std::io;
 use rand::Rng;
 
 
-fn main() {
+fn game(high_score: u32) -> u32 {
     println!("Guess the number!");
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
-    let mut guess_count: i32 = 1;
+    let mut guess_count: u32 = 1;
 
     loop {
         println!("Please input your guess:");
@@ -21,8 +21,6 @@ fn main() {
             Ok(num) => num,
             Err(_) => continue,
         };
-
-        println!("You guessed: {guess}");
 
         match guess.cmp(&secret_number) {
             Ordering::Less => {
@@ -40,4 +38,35 @@ fn main() {
             }
         }
     }
+    let mut new_high_score: u32 = high_score;
+    if high_score > guess_count {
+        new_high_score = guess_count;
+    }
+    new_high_score
+
+}
+
+fn main () {
+    let mut high_score: u32 = 101;
+    let mut repeater = true;
+    while repeater {
+        high_score = game(high_score);
+
+        println!("Would you like to play again? (yes or no)");
+        let mut user_repeating = String::new();
+
+        io::stdin()
+            .read_line(&mut user_repeating)
+            .expect("Failed to read line");
+
+        let repeat_answer = user_repeating.chars().next().unwrap().to_ascii_lowercase();
+
+        if 'y' != repeat_answer {
+            repeater = false;
+        }
+
+    }
+    let score_string: String = high_score.to_string();
+    println!("Your best score is {score_string}!");
+    println!("Play again soon!");
 }
